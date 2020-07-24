f126b1671dc45da9c3d4df5a31e7529e1b0503d2
eck7965 <eck7965@gmail.com>
2020-07-24 11:17:58 +0900

    add scheduler

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/kimeuichan/stock-to-slack
 go 1.12
 
 require (
+	github.com/go-co-op/gocron v0.3.0
 	github.com/spf13/viper v1.7.0
 	golang.org/x/text v0.3.3
 )
--- main.go
+++ main.go
@@ -1,24 +1,32 @@
 package main
 
 import (
+	"github.com/go-co-op/gocron"
 	"github.com/kimeuichan/stock-to-slack/utils"
 	"github.com/kimeuichan/stock-to-slack/utils/client"
 	"github.com/spf13/viper"
+	"time"
 )
 
 func main() {
 	viper.AutomaticEnv()
+	s := gocron.NewScheduler(time.Local)
 
-	nc := client.GetClient("naver")
-	sc := utils.NewSlack(viper.GetString("SLACK_WEBHOOK_URL"))
+	getStock := func() {
+		nc := client.GetClient("naver")
+		sc := utils.NewSlack(viper.GetString("SLACK_WEBHOOK_URL"))
 
-	stockSummary, err := nc.GetStockSummary(viper.GetString("STOCK_NUMBER"))
+		stockSummary, err := nc.GetStockSummary(viper.GetString("STOCK_NUMBER"))
 
-	if err != nil {
-		panic(err)
+		if err != nil {
+			panic(err)
+		}
+
+		if err := sc.SendStock(stockSummary); err != nil {
+			panic(err)
+		}
 	}
 
-	if err := sc.SendStock(stockSummary); err != nil {
-		panic(err)
-	}
+	s.Every(1).Minutes().Do(getStock)
+	s.StartBlocking()
 }
